a9e20b0edfc29a52ac71771027009d9a4bc88f98
오민택 <ohht10004@gmail.com>
2023-03-06 19:29:16 +0900

    Recurrence Plot Dataset Edit Error

--- Model/Kaai_ssh/datasets/Dataset_recurrence.py
+++ Model/Kaai_ssh/datasets/Dataset_recurrence.py
@@ -8,7 +8,7 @@ from torch.utils import data
 import json
 
 
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def rec_plot_module(s, eps=0.01, steps = 255):
     N = s.size
