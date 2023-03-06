36d9ba944609ccde06492e05823dcc6d30610907
오민택 <ohht10004@gmail.com>
2023-03-06 17:10:58 +0900

    fxgfcgfc

--- Model/Kaai_ssh/utils/args.py
+++
@@ -1,8 +0,0 @@
-import yaml
-from config.main_config import data_path
-
-def parse_args():
-    file_name = data_path
-    with open(file_name, 'r', encoding="UTF-8") as f:
-        temp = yaml.load(f, Loader = yaml.FullLoader)
-    return temp
