520b5e2818ff75294f3120d2ccbf996a6c062ec6
오민택 <ohht10004@gmail.com>
2023-03-22 19:52:48 +0900

    Main train file

---
+++ Model/New_train/train.py
@@ -0,0 +1,42 @@
+import torch
+import logging
+import random
+import os
+from config.main_config import *
+from datetime import datetime
+from utils.load_model import Load_model
+from utils.trainer import AETrain, SADTrain, EGOTrain
+
+def main():
+    start = datetime.now()
+    start = start.strftime('%Y-%m-%d %H:%M:%S')
+    result_path = f'{result_path}{start}/'
+
+    if not os.path.isdir(result_path):
+        os.mkdir(result_path)
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    log_file = result_path + f'log_{start}.txt'
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    logger.info(f'Network Name :: {network_name}')
+
+    if not torch.cuda.is_available():
+        device = 'cpu'
+    
+    if pretrain_weight_path:
+        # 모델을 생성하고 가중치를 업로드 한 뒤 모델을 반환
+        flow_model, bbox_model, ego_model, ego_model_ego_train = Load_model(pretrain_weight_path, network_name)
+    else:
+        # AE 모델을 생성하고 학습을 한 뒤 모델을 반환
+        flow_model, bbox_model, ego_model, ego_model_ego_train = AETrain(network_name)
+        pass
+    
+    SADTrain(flow_model, bbox_model, ego_model)
+    EGOTrain(ego_model_ego_train)
+    
