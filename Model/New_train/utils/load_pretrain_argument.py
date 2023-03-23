799aecc4bd6c12a588f9759f3f2cdbeaf6e58662
오민택 <ohht10004@gmail.com>
2023-03-23 20:36:52 +0900

    Fix function name error

--- Model/New_train/utils/load_pretrain_argument.py
+++ Model/New_train/utils/load_pretrain_argument.py
@@ -6,8 +6,8 @@ def load_optimizer(parameters):
     if pretrain_optimzier == 'Adam':
         return optim.Adam(params=parameters, lr = pretrain_lr, weight_decay= pretrain_weight_decay)
 
-def laod_multistep_lr(optimizer):
-    return optim.lr_scheduler.MultiplicativeLR(optimizer, milestones = pretrain_milestone, gamma = pretrain_gamma)
+def load_multistep_lr(optimizer):
+    return optim.lr_scheduler.MultiStepLR(optimizer, milestones = pretrain_milestone, gamma = pretrain_gamma)
 
 def load_criterion():
     if pretrain_criterion == 'MSE':
