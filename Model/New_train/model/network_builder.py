ca25ae03af2e7a959f773f460baae96fe30cc8a7
오민택 <ohht10004@gmail.com>
2023-03-23 20:35:23 +0900

    Add model  code to test. Have to modify model code that can be used at Recurrence plot

--- Model/New_train/model/network_builder.py
+++ Model/New_train/model/network_builder.py
@@ -1,9 +1,7 @@
 import logging
-
+from model.old_seperate_autoencoder_model import ego_model, flow_model, bbox_model
 def network_builder(net_name, ego_only = False):
     logger = logging.getLogger()
     if net_name == 'Recurrence_1':
         logger.info(f'Build Model ::\t{net_name}')
-        # 모델 3가지 호출
-        # if ego_only: return ego
-        pass
+        return bbox_model(), flow_model(), ego_model()
