187486f6cab3fbb36f404e215d2d8f5a0d5e6f93
오민택 <ohht10004@gmail.com>
2023-03-24 20:40:09 +0900

    fix bug

--- Model/New_train/model/network_builder.py
+++ Model/New_train/model/network_builder.py
@@ -1,8 +1,8 @@
 import logging
-from model.old_seperate_autoencoder_model import ego_model, flow_model, bbox_model
+from model.recurrence_dummy_model import dummy_flow, dummy_bbox, dummy_ego
 
 def network_builder(net_name, ego_only = False):
     logger = logging.getLogger()
     if net_name == 'Recurrence_1':
         logger.info(f'Build Model ::\t{net_name}')
-        return bbox_model(), flow_model(), ego_model()
+        return dummy_bbox(), dummy_flow(), dummy_ego()
