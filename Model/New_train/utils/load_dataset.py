0f132bd5b66e4919abef353bb4dd2ed36db75a8e
오민택 <ohht10004@gmail.com>
2023-03-24 20:39:47 +0900

    fix bug

--- Model/New_train/utils/load_dataset.py
+++ Model/New_train/utils/load_dataset.py
@@ -1,7 +1,8 @@
 import torch
 from torch.utils import data
+from config.data_config import data_shuffle, data_batch, num_workers
 
 def dataset_loader(net_name):
     if net_name.split('_')[0] == 'Recurrence':
         from datasets.Dataset_recurrence import Recurrence_Pretrain_DATASET
-        return data.DataLoader(Recurrence_Pretrain_DATASET())
+        return data.DataLoader(Recurrence_Pretrain_DATASET(), batch_size=data_batch, shuffle = data_shuffle, num_workers=num_workers)
