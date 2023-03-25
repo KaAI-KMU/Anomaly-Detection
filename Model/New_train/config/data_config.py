33aadc00ecaf3e923e3a9f60a6668655b4218d59
오민택 <ohht10004@gmail.com>
2023-03-25 22:51:05 +0900

    Add more variance about data

--- Model/New_train/config/data_config.py
+++ Model/New_train/config/data_config.py
@@ -1,3 +1,11 @@
 data_root = 'DATA/'
 data_length = 16
 stride = 1
+
+data_shuffle = True
+data_batch = 32
+num_workers = 0
+
+flow_eps = [0.01, 0.009]
+bbox_eps = [0.0003, 0.0005, 0.0005, 0.0006]
+ego_eps = [0.01, 0.01, 0.01]
