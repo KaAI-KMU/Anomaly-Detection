610e46687daaf0237e1878f323aeaa7257dd7cd3
오민택 <ohht10004@gmail.com>
2023-03-28 17:13:52 +0900

    Edit data weight loader

--- Model/New_train/utils/load_model.py
+++ Model/New_train/utils/load_model.py
@@ -2,19 +2,18 @@ import torch
 from model import network_builder
 import logging
 
-def Load_model(path, net_name, ego_only = False): # path = result/{time}/
-    bbox_model, flow_model, ego_model = network_builder(net_name, ego_only)
+def Load_model(path, net_name): # path = result/{time}/
+    SAD_other, SAD_ego = network_builder(f'{net_name}_SAD')
     logger = logging.getLogger()
     
-    bbox_dict = torch.load(f'{path}/pretrain/bbox.pt')
-    bbox_model.load_state_dict(bbox_dict)
+    other_dict = torch.load(f'{path}/other.pt')
+    SAD_other.load_state_dict(other_dict['net_dict'])
+    SAD_other['c'] = other_dict['c']
 
-    flow_dict = torch.load(f'{path}/pretrain/flow.pt')
-    flow_model.load_state_dict(flow_dict)
-
-    ego_dict = torch.load(f'{path}/pretrain/ego.pt')
-    ego_model.load_state_dict(ego_dict)
+    ego_dict = torch.load(f'{path}/ego.pt')
+    SAD_ego.load_state_dict(ego_dict['net_dict'])
+    SAD_ego['c'] = ego_dict['c']
 
     logger.info(f'Load Model weight ::\t{path}')
     
-    return bbox_model, flow_model, ego_model, ego_model
+    return SAD_other, SAD_ego
