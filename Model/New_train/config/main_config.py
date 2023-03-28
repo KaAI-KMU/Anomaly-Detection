b5b849d1b0270b426a437c32146b3ff7efed00d0
오민택 <ohht10004@gmail.com>
2023-03-28 17:11:39 +0900

    Move config data about train to train.config

--- Model/New_train/config/main_config.py
+++ Model/New_train/config/main_config.py
@@ -3,14 +3,7 @@ network_name = 'Recurrence_1'
 pretrain_weight_path = None
 device= 'cuda'
 
-pretrain_optimzier = 'Adam'
-pretrain_lr = 0.001
-pretrain_weight_decay = 1e-6
-pretrain_milestone = [50]
-pretrain_gamma = 0.1
-pretrain_criterion = 'MSE'
 
-pretrain_epoch = 1
 
 # dataset_name = 'HEVI'
 # net_name = 'Recurrence'
---
+++ Model/New_train/config/train_config.py
@@ -0,0 +1,18 @@
+# Pretrain Section
+pretrain_optimzier = 'Adam'
+pretrain_lr = 0.001
+pretrain_weight_decay = 1e-6
+pretrain_milestone = [50]
+pretrain_gamma = 0.1
+pretrain_criterion = 'MSE'
+
+pretrain_epoch = 1
+
+# SAD Train Section
+train_optimizer = 'Adam'
+train_lr = 0.001
+train_weight_decay = 1e-6
+train_milestone = [50]
+train_gamma = 0.1
+
+train_epoch = 100
