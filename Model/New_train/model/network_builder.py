0d32170d441601958b1641f3b8edd1d23e408a96
오민택 <ohht10004@gmail.com>
2023-03-28 17:12:25 +0900

    Add SAD Model Builder

--- Model/New_train/model/network_builder.py
+++ Model/New_train/model/network_builder.py
@@ -1,8 +1,12 @@
 import logging
 from model.recurrence_dummy_model import dummy_flow, dummy_bbox, dummy_ego
+from model.Recurrence_1 import SAD_other, SAD_ego
 
 def network_builder(net_name, ego_only = False):
     logger = logging.getLogger()
     if net_name == 'Recurrence_1':
         logger.info(f'Build Model ::\t{net_name}')
         return dummy_bbox(), dummy_flow(), dummy_ego()
+    if net_name == 'Recurrence_1_SAD':
+        logger.info(f'Build Model ::\t{net_name}')
+        return SAD_other(), SAD_ego()
