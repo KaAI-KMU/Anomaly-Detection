0e757ac1c1ae060788b26cc0a67519583e818512
MT_Docker <ohht10004@gmail.com>
2023-04-03 12:33:34 +0000

    Edit code that return real model not dummy model

--- Model/New_train/model/network_builder.py
+++ Model/New_train/model/network_builder.py
@@ -1,12 +1,14 @@
 import logging
 from model.recurrence_dummy_model import dummy_flow, dummy_bbox, dummy_ego
-from model.Recurrence_1 import SAD_other, SAD_ego
+
 
 def network_builder(net_name, ego_only = False):
     logger = logging.getLogger()
     if net_name == 'Recurrence_1':
-        logger.info(f'Build Model ::\t{net_name}')
-        return dummy_bbox(), dummy_flow(), dummy_ego()
+        from model.recurrence_1 import flow_ae, bbox_ae, ego_ae
+        logger.info(f'Build Model\t::\t{net_name}')
+        return bbox_ae(), flow_ae(), ego_ae()
     if net_name == 'Recurrence_1_SAD':
-        logger.info(f'Build Model ::\t{net_name}')
-        return SAD_other(), SAD_ego()
+        from model.recurrence_1 import other_SAD, ego_SAD
+        logger.info(f'Build Model\t::\t{net_name}')
+        return other_SAD(), ego_SAD()
