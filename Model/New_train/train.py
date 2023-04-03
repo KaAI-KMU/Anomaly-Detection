a6de0c9e206913eea8001ac3d6d72087a992fac8
MT_Docker <ohht10004@gmail.com>
2023-04-03 12:30:45 +0000

    Complete train code

--- Model/New_train/train.py
+++ Model/New_train/train.py
@@ -9,6 +9,8 @@ from utils.trainer import AETrain, SADTrain, EGOTrain
 from utils.init_model import init_weight
 
 def main():
+    
+    net_name = network_name
     start = datetime.now()
     start = start.strftime('%Y_%m_%d_%H_%M_%S')
     result_path = f'{RESULT_PATH}{start}/'
@@ -25,7 +27,7 @@ def main():
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
-    logger.info(f'Network Name :: {network_name}')
+    logger.info(f'Network Name :: {net_name}')
 
     if not torch.cuda.is_available():
         device = 'cpu'
@@ -33,16 +35,22 @@ def main():
     # 모델들은 CPU에 위치
     if pretrain_weight_path:
         # 모델을 생성하고 가중치를 업로드 한 뒤 모델을 반환
-        flow_model, bbox_model, ego_model, ego_model_ego_train = Load_model(pretrain_weight_path, network_name)
+        flow_model, bbox_model, ego_model, ego_model_ego_train = Load_model(pretrain_weight_path, net_name)
     else:
         # AE 모델을 생성하고 학습을 한 뒤 모델을 반환
-        flow_model, bbox_model, ego_model, ego_model_ego_train = AETrain(network_name, result_path)
-        other_model, ego_model = init_weight(flow_model, bbox_model, ego_model, ego_model_ego_train, network_name)
+        flow_model, bbox_model, ego_model, ego_model_ego_train = AETrain(net_name, result_path)
+        other_model, ego_model = init_weight(flow_model, bbox_model, ego_model, ego_model_ego_train, net_name)
     
-    network_name = f'{network_name}_SAD'
+    net_name = f'{net_name}_SAD'
 
-    SADTrain(other_model, network_name)
-    EGOTrain(ego_model)
+    other_model = SADTrain(other_model, net_name)
+    ego_model = EGOTrain(ego_model, net_name)
+
+    # 저장하기
+    torch.save(other_model.state_dict(), f'{result_path}other_model.pt')
+    torch.save(ego_model.state_dict(), f'{result_path}ego_model.pt')
+    logger.info(f'Train Weight Saved\t::\t{result_path}')
+
     
 if __name__ == '__main__':
     main()
