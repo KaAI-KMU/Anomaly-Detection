b11de92a17c3222d43141b41c34b224d12a8d36f
MT_Docker <ohht10004@gmail.com>
2023-04-05 11:44:34 +0000

    Add split_ratio value to split train and validation data

--- Model/New_train/config/data_config.py
+++ Model/New_train/config/data_config.py
@@ -5,6 +5,7 @@ stride = 1
 data_shuffle = True
 data_batch = 256
 num_workers = 0
+split_ratio = [0.8, 0.2] # train, validation data ratio
 
 flow_eps = [0.01, 0.009]
 bbox_eps = [0.0003, 0.0005, 0.0005, 0.0006]
