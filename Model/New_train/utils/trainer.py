5f01b1728e93de82870457ff66410929ad834baa
MT_Docker <ohht10004@gmail.com>
2023-04-05 11:45:34 +0000

    Add validation part Not sure it work now. It have to be tested before use it

--- Model/New_train/utils/trainer.py
+++ Model/New_train/utils/trainer.py
@@ -33,18 +33,21 @@ def AETrain(net_name, result_path):
     criterion = criterion.to(device)
 
     train_time = time.time()
-    train_generator = dataset_loader(network_name)
+    train_generator, validation_generator = dataset_loader(network_name)
     length = len(train_generator)
+    length_val = len(validation_generator)
     logger.info(f'Pretrain Training Data Length\t::\t{length}')
+    logger.info(f'Pretrain Validation Data Length\t::\t{length_val}')
     logger.info(f'Pretrain Training Epoch\t::\t{pretrain_epoch}')
     for epoch in range(pretrain_epoch):
         bbox_model.train()
         flow_model.train()
         ego_model.train()
+        criterion.train()
 
         #logger.info(f"Pretrain Start\t::\t{epoch+1} epoch")
+        # loader는 epoch 시작할 때 마다 새로 만들어 줘야 그래프가 계속 생김
         loader = tqdm(train_generator, total = length)
-
         
         batch_number = 0
         epoch_time = time.time()
@@ -78,15 +81,55 @@ def AETrain(net_name, result_path):
             optimizer_ego.step()
             ego_avg_loss += ego_loss.mean()
 
-            batch_number += 1
+            batch_number += bbox_data.shape[0]
 
         scheduler_bbox.step()
         scheduler_flow.step()
         scheduler_ego.step()
 
         epoch_time = time.time() - epoch_time
-        logger.info(f'Pretrain AutoEncoder\t::\t{epoch+1}/{pretrain_epoch} :: Train Time :: {epoch_time:.3f}s '
+        logger.info(f'Pretrain AutoEncoder Training\t::\t{epoch+1}/{pretrain_epoch} :: Train Time :: {epoch_time:.3f}s '
                     f'BBox loss :: {bbox_avg_loss/batch_number:.6f} Flos loss :: {flow_avg_loss/batch_number:.6f} Ego Loss :: {ego_avg_loss/batch_number:.6f}')
+
+        loader_val = tqdm(validation_generator, total = length_val)
+
+        bbox_model.eval()
+        flow_model.eval()
+        ego_model.eval()
+        criterion.eval()
+
+        batch_number = 0
+        epoch_time = time.time()
+        
+        bbox_avg_loss = 0
+        flow_avg_loss = 0
+        ego_avg_loss = 0
+        with torch.no_grad():
+            for data in loader_val:
+                bbox_data, flow_data, ego_data = data
+                prediction = bbox_model(bbox_data)
+                bbox_loss = criterion(prediction, bbox_data)
+                bbox_avg_loss += bbox_loss.mean()
+                # Flow Data Train
+                prediction = flow_model(flow_data)
+                flow_loss = criterion(prediction, flow_data)
+                flow_avg_loss += flow_loss.mean()
+                # Ego Data Train
+                prediction = ego_model(ego_data)
+                ego_loss = criterion(prediction, ego_data)
+                ego_avg_loss += ego_loss.mean()
+
+                batch_number += bbox_data.shape[0]
+        epoch_time = time.time() - epoch_time
+        logger.info(f'Pretrain AutoEncoder Validation\t::\t{epoch+1}/{pretrain_epoch} :: Validation Time :: {epoch_time:.3f}s '
+                    f'BBox loss :: {bbox_avg_loss/batch_number:.6f} Flos loss :: {flow_avg_loss/batch_number:.6f} Ego Loss :: {ego_avg_loss/batch_number:.6f}')
+        
+        ################################################
+        # Callback 함수 들어갈 자기
+        # 각 task별로 loss 계산한 다음에 validation loss가 이전보다 낮다면 해당 모델의 weight를 dictionary 타입으로 저장
+        # 나중에 pretrained weight를 저장할 때 해당 dictioanry type weight을 load_state_dict를 통해서 업로드 한 다음에 모델을 저장
+        ###############################################
+
 
     train_time = time.time() - train_time
     logger.info(f'Pretrain Finished\t::\t{train_time}')
@@ -124,21 +167,25 @@ def SADTrain(other_model, net_name):
     schedular_SAD = load_train_multistep_lr(optimizer_SAD)
     
     start_time = time.time()
-    other_model.train()
+    
 
-    train_generator = dataset_loader(net_name=net_name, state = 'SAD')
+    train_generator, validation_generator = dataset_loader(net_name=net_name, state = 'SAD')
 
     center = other_model.c
 
     length = len(train_generator)
-    logger.info(f'SAD Training Data Length :: {length}')
+    length_val = len(validation_generator)
+    logger.info(f'SAD Training Data Length\t::\t{length}')
+    logger.info(f'SAD Validation Data Length\t::\t{length_val}')
     logger.info(f'SAD Training Epoch :: {train_epoch}')
     for epoch in range(train_epoch):
-        
+        other_model.train()
+
+        loader = tqdm(train_generator, total = length)
+
         epoch_loss = 0.0
         n_batch = 0
         epoch_time = time.time()
-        loader = tqdm(train_generator, total = length)
 
         for data in loader:
             bbox, flow, ego, _, label, _ = data
@@ -155,11 +202,39 @@ def SADTrain(other_model, net_name):
             optimizer_SAD.step()
 
             epoch_loss += loss.item()
-            n_batch += 1
+            n_batch += bbox.shape[0]
         schedular_SAD.step()
         epoch_time = time.time() - epoch_time
         logger.info(f'Train SAD :: {epoch+1}/{train_epoch} :: Train Time :: {epoch_time:.3f}s '
                     f'loss :: {epoch_loss / n_batch:.6f}')
+        
+        loader_val = tqdm(validation_generator, total = length_val)
+
+        other_model.eval()
+        epoch_loss = 0.0
+        n_batch = 0
+        epoch_time = time.time()
+        with torch.no_grad():
+            for data in loader_val:
+                bbox, flow, ego, _, label, _ = data
+                label = torch.unsqueeze(label, axis = -1)
+                label = label.to(device)
+                result = other_model(bbox, flow, ego)
+                distance = torch.sum((result - center) ** 2, dim = 1).squeeze()
+
+                loss = torch.where(label == 1, distance, eta * ((distance + 1e-6) ** label.float()))
+                loss = torch.mean(loss)
+
+                epoch_loss += loss.item()
+                n_batch += bbox.shape[0]
+        epoch_time = time.time() - epoch_time
+        logger.info(f'Validation SAD :: {epoch+1}/{train_epoch} :: Validation Time :: {epoch_time:.3f}s '
+                    f'loss :: {epoch_loss / n_batch:.6f}')
+        
+        ######################################################################
+        # 이하 동일
+
+
     start_time = time.time() - start_time
     logger.info(f'SAD Training Time :: {start_time:.3f}s')
     logger.info('SAD Training Finish')
@@ -179,20 +254,24 @@ def EGOTrain(ego_model, net_name):
     schedular_SAD = load_train_multistep_lr(optimizer_SAD)
     
     start_time = time.time()
-    ego_model.train()
+    
 
-    train_generator = dataset_loader(net_name=net_name, state = 'SAD_EGO')
+    train_generator, validation_generator = dataset_loader(net_name=net_name, state = 'SAD_EGO')
 
     center = ego_model.c
     length = len(train_generator)
+    length_val = len(validation_generator)
     logger.info(f'SAD_EGO Training Data Length :: {length}')
+    logger.info(f'SAD_EGO Validation Data Length :: {length_val}')
     logger.info(f'SAD_EGO Training Epoch :: {train_epoch}')
     for epoch in range(train_epoch):
+        ego_model.train()
+
+        loader = tqdm(train_generator, total = length)
         
         epoch_loss = 0.0
         n_batch = 0
         epoch_time = time.time()
-        loader = tqdm(train_generator, total = length)
 
         for data in loader:
             ego, _, label = data
@@ -208,11 +287,37 @@ def EGOTrain(ego_model, net_name):
             optimizer_SAD.step()
 
             epoch_loss += loss.item()
-            n_batch += 1
+            n_batch += ego.shape[0]
         schedular_SAD.step()
         epoch_time = time.time() - epoch_time
         logger.info(f'Train SAD_EGO :: {epoch+1}/{train_epoch} :: Train Time :: {epoch_time:.3f}s '
                     f'loss :: {epoch_loss / n_batch:.6f}')
+        
+        loader_val = tqdm(validation_generator, total = length_val)
+
+        ego_model.eval()
+
+        epoch_loss = 0.0
+        n_batch = 0
+        epoch_time = time.time()
+
+        with torch.no_grad():
+            for data in loader_val:
+                ego, _, label = data
+                label = label.to(device)
+                result = ego_model(ego)
+                distance = torch.sum((result - center) ** 2, dim = 1).squeeze()
+                loss = torch.where(label == 1, distance, eta * ((distance + 1e-6) ** label.float()))
+                loss = torch.mean(loss)
+                epoch_loss += loss.item()
+                n_batch += ego.shape[0]
+                
+        epoch_time = time.time() - epoch_time
+        logger.info(f'Validation SAD_EGO :: {epoch+1}/{train_epoch} :: Train Time :: {epoch_time:.3f}s '
+                    f'loss :: {epoch_loss / n_batch:.6f}')
+
+
+
     start_time = time.time() - start_time
     logger.info(f'SAD_EGO Training Time :: {start_time:.3f}s')
     logger.info('SAD_EGO Training Finish')
