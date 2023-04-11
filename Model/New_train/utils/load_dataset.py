fe3c83de91b297dd5a1dd0eecb231b9d4706e6d8
MT_Docker <ohht10004@gmail.com>
2023-04-11 05:49:34 +0000

    Fix code for  initializing data loader

--- Model/New_train/utils/load_dataset.py
+++ Model/New_train/utils/load_dataset.py
@@ -1,7 +1,7 @@
 from torch.utils import data
 from config.data_config import data_shuffle, data_batch, num_workers, split_ratio
 
-def dataset_loader(net_name, state = 'Pretrain'):
+def dataset_loader(net_name, state = 'Pretrain', purpose = 'Train'):
     if net_name.split('_')[0] == 'Recurrence' and state == 'Pretrain':
         from datasets.Dataset_pickled_recurrence import Recurrence_Pretrain_DATASET
         train = Recurrence_Pretrain_DATASET()
@@ -13,8 +13,12 @@ def dataset_loader(net_name, state = 'Pretrain'):
     elif net_name.split('_')[0] == 'Recurrence' and state == 'SAD_EGO':
         from datasets.Dataset_pickled_recurrence import Recurrence_SAD_EGO_DATASET
         train = Recurrence_SAD_EGO_DATASET()
-        
-    train, val = data.random_split(train, split_ratio)
-    train = data.DataLoader(train, batch_size=data_batch, shuffle = data_shuffle, num_workers=num_workers)
-    val = data.DataLoader(val, batch_size=data_batch, shuffle = data_shuffle, num_workers=num_workers)
-    return train, val
+    
+    if purpose == 'Train':
+        train, val = data.random_split(train, split_ratio)
+        train = data.DataLoader(train, batch_size=data_batch, shuffle = data_shuffle, num_workers=num_workers)
+        val = data.DataLoader(val, batch_size=data_batch, shuffle = data_shuffle, num_workers=num_workers)
+        return train, val
+    elif purpose == 'Init':
+        train = data.DataLoader(train, batch_size=data_batch, shuffle = data_shuffle, num_workers=num_workers)
+        return train 
