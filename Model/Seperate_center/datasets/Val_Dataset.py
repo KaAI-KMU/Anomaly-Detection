4afdce644dffb6c80f0f7715190c39a40a8822d5
MT_Docker <ohht10004@gmail.com>
2023-05-10 03:47:28 +0000

    Make data loader more efficiently

--- Model/Seperate_center/datasets/Val_Dataset.py
+++ Model/Seperate_center/datasets/Val_Dataset.py
@@ -22,29 +22,39 @@ class Recurrence_SAD_DATASET(data.Dataset):
             # for each car in dataset, we split to several trainig samples
             
             #load data and label
-            data_path = os.path.join(self.data_root, name)
-            data = pkl.load(open(data_path, 'rb'))
+            self.all_inputs.append(os.path.join(self.data_root, name))
             
-            input_bbox = data['bbox']
-            input_flow = data['flow']
-            input_ego = data['ego_motion']# [yaw, x, z]
-            video_name = '_'.join(name.split('_')[:2])
-            label = data['label']
-            frame_id = [data['frame_id']]
+            #data_path = os.path.join(self.data_root, name)
             
-            self.all_inputs.append([input_bbox, input_flow, input_ego, video_name, label, frame_id])
+
+            #data = pkl.load(open(data_path, 'rb'))
+            
+            #input_bbox = data['bbox']
+            #input_flow = data['flow']
+            #input_ego = data['ego_motion']# [yaw, x, z]
+            #video_name = '_'.join(name.split('_')[:2])
+            #label = data['label']
+            #frame_id = [data['frame_id']]
+            
+            #self.all_inputs.append([input_bbox, input_flow, input_ego, video_name, label, frame_id])
             
     def __len__(self):
         return len(self.all_inputs)
     
     def __getitem__(self, index):
-        input_bbox, input_flow, input_ego_motion, video_name, label, frame_id= self.all_inputs[index]
-        input_bbox = torch.FloatTensor(input_bbox/255).to(device)
-        input_flow = torch.FloatTensor(input_flow/255).to(device)
-        input_ego_motion = torch.FloatTensor(input_ego_motion/255).to(device)
-        frame_id = torch.IntTensor(frame_id)
+        
+        path = self.all_inputs[index]
+        data = pkl.load(open(path, 'rb'))
 
-        return input_bbox, input_flow, input_ego_motion, video_name, label, frame_id
+        input_bbox = torch.FloatTensor(data['bbox']/255).to(device)
+        input_flow = torch.FloatTensor(data['flow']/255).to(device)
+        input_ego = torch.FloatTensor(data['ego_motion']/255).to(device) #yaw, x, z]
+        label = torch.IntTensor([data['label']])
+        frame_id = torch.IntTensor([data['frame_id']])
+        video_name = '_'.join(path.split('/')[-1].split('_')[:2])
+
+
+        return input_bbox, input_flow, input_ego, video_name, label, frame_id
     
 
 class Recurrence_SAD_EGO_DATASET(data.Dataset):
