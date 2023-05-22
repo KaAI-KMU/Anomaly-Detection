b73e4e0ebf75f9f7f12660b2fa69974d24908637
MT_Docker <ohht10004@gmail.com>
2023-05-22 10:01:32 +0000

    Change code that make variable channel feature

--- Model/linear_train/model/conv_1.py
+++ Model/linear_train/model/conv_1.py
@@ -1,26 +1,29 @@
 from torch import nn
-
+import numpy as np
 class CONV_MODEL(nn.Module):
     """Some Information about CONV_MODEL"""
-    def __init__(self, feature = 9):
+    def __init__(self, feature):
         super(CONV_MODEL, self).__init__()
+
+        numbers = np.linspace(16, feature, num = 5, dtype = np.int32)
+
         self.encoder = nn.Sequential(
-                        nn.Conv1d(16, 25, 3),
+                        nn.Conv1d(16, numbers[1], 3),
                         nn.ReLU(),
-                        nn.Conv1d(25, 36, 3),
+                        nn.Conv1d(numbers[1], numbers[2], 3),
                         nn.ReLU(),
-                        nn.Conv1d(36, 49, 3),
+                        nn.Conv1d(numbers[2], numbers[3], 3),
                         nn.ReLU(),
-                        nn.Conv1d(49, 64, 3),
+                        nn.Conv1d(numbers[3], numbers[4], 3),
                     )
         self.decoder = nn.Sequential(
-                        nn.ConvTranspose1d(64, 49, 3),
+                        nn.ConvTranspose1d(numbers[4], numbers[3], 3),
                         nn.ReLU(),
-                        nn.ConvTranspose1d(49, 36, 3),
+                        nn.ConvTranspose1d(numbers[3], numbers[2], 3),
                         nn.ReLU(),
-                        nn.ConvTranspose1d(36, 25, 3),
+                        nn.ConvTranspose1d(numbers[2], numbers[1], 3),
                         nn.ReLU(),
-                        nn.ConvTranspose1d(25, 16, 3),
+                        nn.ConvTranspose1d(numbers[1], 16, 3),
                         nn.Sigmoid()
         )
 
@@ -33,18 +36,21 @@ class CONV_MODEL(nn.Module):
 
 class CONV_MODEL_SAD(nn.Module):
     """Some Information about CONV_MODEL_SAD"""
-    def __init__(self, feature = 9):
-        super(CONV_MODEL_SAD, self).__init__()
-        self.encoder = nn.Sequential(
-                        nn.Conv1d(16, 25, 3),
-                        nn.ReLU(),
-                        nn.Conv1d(25, 36, 3),
-                        nn.ReLU(),
-                        nn.Conv1d(36, 49, 3),
-                        nn.ReLU(),
-                        nn.Conv1d(49, 64, 3),
-                    )
+    def __init__(self, feature):
 
+        super(CONV_MODEL_SAD, self).__init__()
+
+        numbers = np.linspace(16, feature, num = 5, dtype = np.int32)
+        
+        self.encoder = nn.Sequential(
+                        nn.Conv1d(16, numbers[1], 3),
+                        nn.ReLU(),
+                        nn.Conv1d(numbers[1], numbers[2], 3),
+                        nn.ReLU(),
+                        nn.Conv1d(numbers[2], numbers[3], 3),
+                        nn.ReLU(),
+                        nn.Conv1d(numbers[3], numbers[4], 3),
+                    )
         self.c = None
 
     def forward(self, data):
