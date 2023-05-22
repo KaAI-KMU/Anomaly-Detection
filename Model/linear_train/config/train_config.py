b8f854db53c47e318ecdbb227bb28f722e68328e
MT_Docker <ohht10004@gmail.com>
2023-05-22 07:29:21 +0000

    Config files

---
+++ Model/linear_train/config/train_config.py
@@ -0,0 +1,21 @@
+# Pretrain Section
+pretrain_optimzier = 'Adam'
+pretrain_lr = 0.001
+pretrain_weight_decay = 1e-6
+pretrain_milestone = [50]
+pretrain_gamma = 0.1
+pretrain_criterion = 'MSE'
+
+pretrain_epoch = 75
+
+# SAD Train Section
+train_optimizer = 'Adam'
+train_lr = 0.001
+train_weight_decay = 1e-6
+train_milestone = [50]
+train_gamma = 0.1
+
+train_epoch = 100
+eta = 1.0
+
+CALLBACK = True
