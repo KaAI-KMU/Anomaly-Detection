a37e95f1f6b7cea700065990a13c3b81b20633b6
MT_Docker <ohht10004@gmail.com>
2023-05-22 07:31:00 +0000

    Callback function that return minimum loss model

---
+++ Model/linear_train/utils/callback_function.py
@@ -0,0 +1,34 @@
+import logging
+
+class best_weight_callback:
+    def __init__(self, name, center, debug = False, ):
+        self.weight = None
+        self.c = None
+        self.loss = 1e9
+        self.task = name
+        self.debug = debug
+        self.center = center
+    def add(self, model, loss_value):
+        # 이전에 best_weight가 없는 경우
+        if self.weight == None:
+            logging.info(f'CallBack_Function\tBest weight\tFisrt {self.task} Model')
+            self.loss = loss_value
+            self.weight = model.state_dict()
+            if self.center:
+                self.c = model.c
+        # 이전의 loss값이 이번의 loss값 보다 큰 경우
+        elif self.loss > loss_value:
+            logging.info(f'CallBack_Function\tBest weight\tBest {self.task} Model Detected\t::\tLoss :: {self.loss} -> {loss_value}')
+            self.loss = loss_value
+            self.weight = model.state_dict()
+            if self.center:
+                self.c = model.c
+    def get_best_model(self, model):
+        model.load_state_dict(self.weight)
+        if self.center:
+            model.c = self.c
+        logging.info(f'CallBack_Function\tBest weight\tReturn Best {self.task} Model :: loss = {self.loss}')
+        if self.debug:
+            return model, self.loss
+        else:
+            return model
